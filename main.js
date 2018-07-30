9a01bf55f2b824655d1af57b42f5424315342d18
Praneeth Thalla <pthalla1@gmail.com>
2018-07-30 03:45:55 -0400

    Worked on material design of About Me

--- main.js
+++ main.js
@@ -18,38 +18,27 @@ $(function() {
   })
   
   $("#thallaio_img").click(function() {
-    $("#thallaio_text").animate({opacity: "show"}, 400);
-    $("#thallaio_desc").animate({opacity: "show"}, 400);
-    $("#thallaio_line").animate({opacity: "show"}, 400);
+    // $("#thallaio_overlay").animate({opacity: "show"}, 400);
+    $("#thallaio_overlay").toggle("scale");
   })
 
   $("#upward_img").click(function() {
-    $("#upward_text").animate({opacity: "show"}, 400);
-    $("#upward_desc").animate({opacity: "show"}, 400);
-    $("#upward_line").animate({opacity: "show"}, 400);
+    $("#upward_overlay").animate({opacity: "show"}, 400);
   })
 
   $("#lebot_img").click(function() {
-    $("#lebot_text").animate({opacity: "show"}, 400);
-    $("#lebot_desc").animate({opacity: "show"}, 400);
-    $("#lebot_line").animate({opacity: "show"}, 400);
+    $("#lebot_overlay").animate({opacity: "show"}, 400);
   })
 
   $("#thallaio_img").mouseleave(function() {
-    $("#thallaio_text").animate({opacity: "hide"}, 400);
-    $("#thallaio_desc").animate({opacity: "hide"}, 400);
-    $("#thallaio_line").animate({opacity: "hide"}, 400);
+    $("#thallaio_overlay").toggle("scale");
   });
 
   $("#upward_img").mouseleave(function() {
-    $("#upward_text").animate({opacity: "hide"}, 400);
-    $("#upward_desc").animate({opacity: "hide"}, 400);
-    $("#upward_line").animate({opacity: "hide"}, 400);  
+    $("#upward_overlay").animate({opacity: "hide"}, 400);
   });
   $("#lebot_img").mouseleave(function() {
-    $("#lebot_text").animate({opacity: "hide"}, 400);
-    $("#lebot_desc").animate({opacity: "hide"}, 400);
-    $("#lebot_line").animate({opacity: "hide"}, 400);
+    $("#lebot_overlay").animate({opacity: "hide"}, 400);
   });
 
 });

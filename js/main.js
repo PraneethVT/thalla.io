d4c2ac240346054242bae34c4c29b92c8ef527c3
Praneeth Thalla <pthalla1@gmail.com>
2018-08-23 13:17:24 -0400

    added ScrollReveal library instead of my own custom scroll js

--- js/main.js
+++ js/main.js
@@ -11,20 +11,99 @@ $(function() {
   })
 
   /* Element scroll in */
-  var documentElement = $(document);
-  var fadeInScrollElement = $(".fadein_with_scroll");
-  documentElement.on('scroll', function() {
-    fadeInScrollElement.each(function() {
-      var $this = $(this);
-      var answer = ($this.offset().top - documentElement.scrollTop()) / $(window).height();
+  /* Default page wise */
+  ScrollReveal().reveal('.fadein_with_scroll', 
+  {
+    distance: "2em",
+    origin: "bottom",
+    viewFactor: "0.1"
+  });
 
-      if (answer > 0 && answer < 0.9) {
-        $this.animate({marginTop: 0, opacity: 1}, 400);
-      }
-    })
-  })
+  /* About Me */
+  ScrollReveal().reveal('.about_me_header', 
+  {
+    distance: "2em",
+    origin: "right",
+    viewFactor: "0.9"
+  });
+  ScrollReveal().reveal('#about_me_header_first', 
+  {
+    distance: "2em",
+    origin: "left",
+    viewFactor: "0.9"
+  });
+  
+  ScrollReveal().reveal('#about_me_header_last', 
+  {
+    distance: "2em",
+    origin: "left",
+    viewFactor: "0.9"
+  });
 
-  /* Image overlay animations */
+  /* Projects */
+  ScrollReveal().reveal('#projects_header', 
+  {
+    distance: "2em",
+    origin: "left",
+    viewFactor: "0.9"
+  });
+
+  /* Experiences */
+  ScrollReveal().reveal('#experiences_header', 
+  {
+    distance: "1em",
+    origin: "bottom",
+    viewFactor: "0.9"
+  });
+
+  ScrollReveal().reveal('#experiences_line', 
+  {
+    distance: "1em",
+    origin: "bottom",
+    viewFactor: "0.9"
+  });
+
+  ScrollReveal().reveal('.experience_img', 
+  {
+    distance: "2em",
+    origin: "bottom",
+    viewFactor: "0.3"
+  });
+
+  ScrollReveal().reveal('.experience_text_container', 
+  {
+    distance: "2em",
+    origin: "bottom",
+    viewFactor: "0.3"
+  });
+  /* Footer */
+  ScrollReveal().reveal('#footer_message', 
+  {
+    distance: "1em",
+    origin: "bottom",
+    viewFactor: "0.5"
+  });
+  ScrollReveal().reveal('#footer_email', 
+  {
+    distance: "1em",
+    origin: "bottom",
+    viewFactor: "0.5"
+  });
+  ScrollReveal().reveal('#footer_copyright', 
+  {
+    distance: "1em",
+    origin: "bottom",
+    viewFactor: "0.5"
+  });
+
+
+
+
+
+
+
+
+  /* Project image overlay animations */
   var thallaioImg = $("#thallaio_img");
   var thallaioOverlay = $("#thallaio_overlay");
   var upwardImg = $("#upward_img");
